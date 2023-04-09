b9c6d6fa371362faddadf1121f60e52e16b35b0d
Brigurde <48880155+Brigurde@users.noreply.github.com>
2023-04-09 19:02:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_projet_4a
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_projet_4a/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_projet_4a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
